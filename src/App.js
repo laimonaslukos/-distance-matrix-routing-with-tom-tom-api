beea8fd9947627cb214974c0f34c182c6f15a2f7
„Laimonas <laimonas.lukosevicius@gmail.com>
2021-09-17 10:45:23 +0300

    Get map from tom tom API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
+import * as tt from '@tomtom-international/web-sdk-maps'
 
 const App = () => {
-  return <div className="App">Hello World</div>;
+  const mapElement = useRef()
+  const [map, setMap] = useState({})
+
+useEffect(()=> {
+  let map = tt.map({
+    key: process.env.REACT_APP_TOM_TOM_API_KEY,
+    container: mapElement.current,
+  })
+setMap(map)
+
+}, [])
+
+
+  return <div className="App">
+    <div ref={mapElement}>
+
+    </div>
+  </div>;
 };
 
 export default App;
